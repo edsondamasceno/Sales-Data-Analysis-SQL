8bf185196f8020b9f08995f68e9121b263067457
Edson Damasceno <edsondamasceno22@gmail.com>
2026-02-15 18:55:01 -0300

    Script para criação do banco de dados

---
+++ 01_create_database.sql
@@ -0,0 +1,13 @@
+/*
+Sales Data Analysis with SQL
+
+Este projeto utiliza o conjunto de dados "sales dataset",
+que apresenta dados de vendas sintéticos gerados para fins de simulação.
+
+O conjunto de dados pode ser acessado no Kaggle pelo link:
+https://www.kaggle.com/datasets/vinothkannaece/sales-dataset
+*/
+
+-- Criando o Database
+
+CREATE DATABASE vendas_db;
